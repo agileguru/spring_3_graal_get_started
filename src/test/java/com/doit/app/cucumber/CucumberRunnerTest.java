fec8470ceba57d0583b047e90c80bad4e5e8a929
Guru Raghupathy <guru@doit-intl.com>
2023-08-30 17:32:31 +0100

    Added report configuration

--- src/test/java/com/doit/app/cucumber/CucumberRunnerTest.java
+++ src/test/java/com/doit/app/cucumber/CucumberRunnerTest.java
@@ -13,6 +13,7 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @SelectDirectories("src/test/resources/features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.doit.app.cucumber")
 @ConfigurationParameter(key = Constants.PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty,html:target/reports/cucumber/report.html")
 public class CucumberRunnerTest {
 
 }
